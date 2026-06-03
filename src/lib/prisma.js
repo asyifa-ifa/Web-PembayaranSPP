12e1b42c7aa25d58c4b48cae04986c3caeb154d3
asyifa_ifa <ifa935791@gmail.com>
2026-06-04 06:47:39 +0700

    fix:update lib prisma

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -2,31 +2,6 @@ import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = global
 
-const prisma = globalForPrisma.prisma ?? new PrismaClient()
+globalForPrisma.prisma = globalForPrisma.prisma || new PrismaClient()
 
-// Pasang middleware hanya jika belum dipasang
-if (!prisma._middlewares?.length) {
-  prisma.$use(async (params, next) => {
-    const result = await next(params)
-
-    if (params.model === "Student" && params.action === "update") {
-      const data      = params.args?.data
-      const studentId = params.args?.where?.id
-
-      if (studentId && data?.email) {
-        await prisma.login.updateMany({
-          where: { studentId },
-          data:  { email: data.email },
-        })
-      }
-    }
-
-    return result
-  })
-}
-
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma
-}
-
-export default prisma
+export default globalForPrisma.prisma
