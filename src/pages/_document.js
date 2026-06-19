ff9f4d8e72aa5b81cea4288750340ae151e21f24
asyifa-ifa <ifa935791@gmail.com>
2026-06-19 18:16:48 +0700

    perbaikan logo

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,8 +2,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="id">
       <Head>
+        <link rel="icon" href="/logo-sibatamu.png" />
         <script
           src="https://app.sandbox.midtrans.com/snap/snap.js"
           data-client-key={process.env.NEXT_PUBLIC_MIDTRANS_CLIENT_KEY}
